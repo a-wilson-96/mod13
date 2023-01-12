e62467cea77614a6171d7ef80c5b52a12c86a4d3
Aaron Wilson <aaronmwilson96@gmail.com>
2023-01-12 11:02:08 -0900

    added: routes

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  forgeinKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Categories have many Products
+Category.hasMany(Tag, {
+  foreignKey: 'category_id',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  forgeinKey: 'product_id',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
